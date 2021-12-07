4394712b0abe42775e6e2d19bc56c98b2658b8ca
owen <owen@joget.org>
2021-12-07 15:55:25 +0800

    Fixed: Security nonce not verified correctly.

--- src/main/java/org/joget/plugin/SectionTabs.java
+++ src/main/java/org/joget/plugin/SectionTabs.java
@@ -23,7 +23,7 @@ public class SectionTabs extends Element implements FormBuilderPaletteElement, F
 
     @Override
     public String getVersion() {
-        return "7.0.6";
+        return "7.0.7";
     }
 
     @Override
--- src/main/java/org/joget/plugin/SectionTabsChild.java
+++ src/main/java/org/joget/plugin/SectionTabsChild.java
@@ -30,7 +30,7 @@ public class SectionTabsChild extends Section implements PluginWebSupport{
     
     @Override
     public String getVersion() {
-        return "7.0.6";
+        return "7.0.7";
     }
     
     @Override
@@ -114,7 +114,7 @@ public class SectionTabsChild extends Section implements PluginWebSupport{
         formData.setActivityId(StringUtil.escapeString(request.getParameter("activityId"), StringUtil.TYPE_HTML, null));
         
         String nonce = request.getParameter("_nonce");
-        if (!SecurityUtil.verifyNonce(nonce, new String[]{"SectionTabsChild", request.getParameter("_elementId"), nonce})) {
+        if (!SecurityUtil.verifyNonce(nonce, new String[]{"SectionTabsChild", request.getParameter("_elementId")})) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return;
         }
